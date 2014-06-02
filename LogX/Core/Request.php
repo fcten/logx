949e93e05d45dbc2467817f935858a6ecdf0b0f7
fcten <huangke5555@163.com>
2014-06-03 02:04:53 +0800

    修正了一处导致Cookie:isMobile被反复设置的错误

--- LogX/Core/Request.php
+++ LogX/Core/Request.php
@@ -11,6 +11,8 @@ class Request {
 
 	private static $_prefix = '';
 
+	private static $_isMobile = NULL;
+
 	public static function G( $key, $type = 'int' ) {
 		return self::GPCS( isset($_GET[$key])?$_GET[$key]:NULL, $type );
 	}
@@ -101,26 +103,26 @@ class Request {
 	 * @return bool
 	 */
 	public static function isMobile() {
-		if( Request::C( 'isMobile', 'string' ) == 'TRUE' ) {
+		if( self::$_isMobile === TRUE || Request::C( 'isMobile', 'string' ) == 'TRUE' ) {
 			return TRUE;
-		} elseif( Request::C( 'isMobile', 'string' ) == 'FALSE' ) {
+		} elseif( self::$_isMobile === FALSE || Request::C( 'isMobile', 'string' ) == 'FALSE' ) {
 			return FALSE;
 		}
 		$user_agent = $_SERVER['HTTP_USER_AGENT'];
 		$mobile_agents = Array("240x320","acer","acoon","acs-","abacho","ahong","airness","alcatel","amoi","android","anywhereyougo.com","applewebkit/525","applewebkit/532","asus","audio","au-mic","avantogo","becker","benq","bilbo","bird","blackberry","blazer","bleu","cdm-","compal","coolpad","danger","dbtel","dopod","elaine","eric","etouch","fly ","fly_","fly-","go.web","goodaccess","gradiente","grundig","haier","hedy","hitachi","htc","huawei","hutchison","inno","ipad","ipaq","ipod","jbrowser","kddi","kgt","kwc","lenovo","lg ","lg2","lg3","lg4","lg5","lg7","lg8","lg9","lg-","lge-","lge9","longcos","maemo","mercator","meridian","micromax","midp","mini","mitsu","mmm","mmp","mobi","mot-","moto","nec-","netfront","newgen","nexian","nf-browser","nintendo","nitro","nokia","nook","novarra","obigo","palm","panasonic","pantech","philips","phone","pg-","playstation","pocket","pt-","qc-","qtek","rover","sagem","sama","samu","sanyo","samsung","sch-","scooter","sec-","sendo","sgh-","sharp","siemens","sie-","softbank","sony","spice","sprint","spv","symbian","tablet","talkabout","tcl-","teleca","telit","tianyu","tim-","toshiba","tsm","up.browser","utec","utstar","verykool","virgin","vk-","voda","voxtel","vx","wap","wellco","wig browser","wii","windows ce","wireless","xda","xde","zte");
-		$is_mobile = false;
+		self::$_isMobile = FALSE;
 		foreach( $mobile_agents as $device ) {
 			if( stristr( $user_agent, $device ) ) {
-				$is_mobile = true;
+				self::$_isMobile = TRUE;
 				break;
 			}
 		}
-		if( $is_mobile ) {
+		if( self::$_isMobile ) {
 			Response::setCookie( 'isMobile', 'TRUE', time()+3600*24*365 );
 		} else {
 			Response::setCookie( 'isMobile', 'FALSE', time()+3600*24*365 );
 		}
-		return $is_mobile;
+		return self::$_isMobile;
 	}
 
 	public static function getDomain() {
